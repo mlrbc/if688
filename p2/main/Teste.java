89794ee39bc3646933cef46c8bcea0d8f79718ae
vrm <vrm@cin.ufpe.br>
2017-04-29 22:41:29 -0300

    Melhorado Teste.java

--- p2/main/Teste.java
+++ p2/main/Teste.java
@@ -5,22 +5,37 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
+import ast.*;
+import visitor.*;
+
+import main.*;
+
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 
 public class Teste {
-	public static void main(String[] args) throws IOException {
-		
-		File folder = new File("testes");
-		File[] files = folder.listFiles();
-		for (int i=0; i< files.length; i++){
-			System.out.println("Testando " + files[i]);
-			InputStream stream = new FileInputStream(files[i]);
-			ANTLRInputStream input = new ANTLRInputStream(stream);
-			minijavaLexer lexer = new minijavaLexer(input);
-			CommonTokenStream token = new CommonTokenStream(lexer);
-			minijavaParser parser = new minijavaParser(token);
-			parser.goal();
+	public static void main(String[] args) {
+		try {
+			File folder = new File("testes");
+			File[] files = folder.listFiles();
+			int num = 0;
+			if (files != null) num = files.length;
+			for (int i=0; i < num; i++){
+				System.out.println("Testando " + files[i]);
+				InputStream stream = new FileInputStream(files[i]);
+				ANTLRInputStream input = new ANTLRInputStream(stream);
+				minijavaLexer lexer = new minijavaLexer(input);
+				CommonTokenStream token = new CommonTokenStream(lexer);
+				minijavaParser parser = new minijavaParser(token);
+				
+				Transforma t = new Transforma();
+				Program prog = t.visitGoal(parser.goal());
+				prog.accept(new PrettyPrintVisitor());
+			}
+		} catch (FileNotFoundException e){
+			System.out.println("Erro com a entrada de teste. " + e);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
